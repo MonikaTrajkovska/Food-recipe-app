77f9d78d14d485ebedcb25edb746c9aece86ea9a
MonikaTrajkovska <49168201+MonikaTrajkovska@users.noreply.github.com>
2020-09-09 18:49:18 +0200

    Updates

---
+++ precache-manifest.2f3de84cf7192f6fa7f5cde2d8cc804c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f1101da87d9604784d3",
+    "url": "/Food-recipe-app/static/css/main.d8fbd096.chunk.css"
+  },
+  {
+    "revision": "6f1101da87d9604784d3",
+    "url": "/Food-recipe-app/static/js/main.6f1101da.chunk.js"
+  },
+  {
+    "revision": "0e4541c721d8b748f101",
+    "url": "/Food-recipe-app/static/js/1.0e4541c7.chunk.js"
+  },
+  {
+    "revision": "bb11710485e6e082e091",
+    "url": "/Food-recipe-app/static/js/runtime~main.bb117104.js"
+  },
+  {
+    "revision": "4bc356e23158c76610727ec4dce276c9",
+    "url": "/Food-recipe-app/index.html"
+  }
+];
